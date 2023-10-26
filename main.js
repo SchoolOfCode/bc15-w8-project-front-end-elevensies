d1daee8b0e451dd316b71a2bfa15bf8767113455
unknown <simionescuionut1@gmail.com>
2023-10-26 10:04:11 +0100

    burger progress

---
+++ main.js
@@ -0,0 +1,8 @@
+function openMenu() {
+  const x = document.getElementById("myLinks");
+  if (x.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
