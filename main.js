78a3f4dfc73bf813abd2b17ab2b32042d6ce9295
Adam Hale <adam.hale92@googlemail.com>
2023-10-29 23:41:17 +0000

    added project information screen as well as updating subtitle on filter

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const projectsList = document.querySelector(".projects-list");
 const logoButton = document.querySelector(".logo");
+const subtitle = document.querySelector("#subtitle");
 const HTMLButton = document.querySelector("#HTML");
 const CSSButton = document.querySelector("#CSS");
 const JSButton = document.querySelector("#JavaScript");
@@ -10,6 +11,14 @@ const DifficultButton = document.querySelector("#Difficult");
 
 async function getAllProjects() {
   const response = await fetch("http://localhost:3000/projects", {
+    method: "GET",
+  });
+  const data = await response.json();
+  return data.data;
+}
+
+async function getProjectById(id) {
+  const response = await fetch(`http://localhost:3000/projects/${id}`, {
     method: "GET",
   });
   const data = await response.json();
@@ -66,6 +75,80 @@ function displaySearchData(searchData) {
     dataContainer.appendChild(projectText);
     listItem.appendChild(dataContainer);
     projectsList.appendChild(listItem);
+
+    listItem.addEventListener("click", async () => {
+      getAndDisplayDetail(searchData[i].id);
+    });
+
+    subtitle.innerHTML = "Suggested Projects";
+  }
+}
+
+function displayDetail(searchData) {
+  for (let i = 0; i < searchData.length; i++) {
+    //create list item
+    const listItem = document.createElement("li");
+    //create new detail container div
+    const detailContainer = document.createElement("div");
+    //set class of new list item to detail-container createdData
+    detailContainer.className = "detail-container createdData";
+    //create detail-title div
+    const detailTitle = document.createElement("div");
+    // set class of new div
+    detailTitle.className = "detail-title createdData";
+    //create h3 element
+    const detailTitleH3 = document.createElement("h3");
+    // set content of h3 element
+    detailTitleH3.innerHTML = searchData[i].name;
+    //create p element
+    const detailDescription = document.createElement("p");
+    // set content of p element
+    detailDescription.textContent = searchData[i].long_description;
+    // create image container
+    const detailImage = document.createElement("div");
+    // set class of new div
+    detailImage.className = "detail-image createdData";
+    // create img tag
+    const suggestedImage = document.createElement("img");
+    // set class of new div
+    suggestedImage.className = "suggested-image createdData";
+    // set src for image
+    suggestedImage.setAttribute("src", "./Images/login_ss.webp");
+    // set alt text
+    suggestedImage.alt = "project preview";
+    //new URL called div
+    const detailURL = document.createElement("div");
+    //set class to detail-url
+    detailURL.className = "detail-url createdData";
+    //set inner text
+    detailURL.innerHTML = `${searchData[i].url}`;
+    // create div element
+    const detailLang = document.createElement("div");
+    // set class and innerHTML
+    detailLang.className = "detail-lang createdData";
+    detailLang.innerHTML = `${searchData[i].language}`;
+    // create difficulty div
+    const detailDifficulty = document.createElement("div");
+    // set class and innerHTML
+    detailDifficulty.className = "detail-difficulty createdData";
+    detailDifficulty.innerHTML = `${searchData[i].difficulty}`;
+    // create div detail inline
+    const detailInline = document.createElement("div");
+    detailInline.className = "detail-inline createdData";
+    //build the div structure
+    detailTitle.appendChild(detailTitleH3);
+    detailTitle.appendChild(detailDescription);
+    detailImage.appendChild(suggestedImage);
+    detailContainer.appendChild(detailTitle);
+    detailContainer.appendChild(detailImage);
+    detailContainer.appendChild(detailURL);
+    detailInline.appendChild(detailLang);
+    detailInline.appendChild(detailDifficulty);
+    detailContainer.appendChild(detailInline);
+    listItem.appendChild(detailContainer);
+    projectsList.appendChild(listItem);
+
+    subtitle.innerHTML = "Project Details";
   }
 }
 
@@ -73,6 +156,12 @@ async function getAndDisplayAllData() {
   const returnData = await getAllProjects();
   deleteOldData();
   displaySearchData(returnData);
+}
+
+async function getAndDisplayDetail(id) {
+  const returnData = await getProjectById(id);
+  deleteOldData();
+  displayDetail(returnData);
 }
 
 // Create a function that deletes the suggested projects
@@ -113,11 +202,13 @@ async function getAndDisplayDifficulty(Level) {
   const returnData = await getProjectsByDifficulty(Level);
   deleteOldData();
   displaySearchData(returnData);
+  subtitle.innerHTML = `${Level} Projects`;
 }
 async function getAndDisplayLanguage(Lang) {
   const returnData = await getProjectsByLanguage(Lang);
   deleteOldData();
   displaySearchData(returnData);
+  subtitle.innerHTML = `${Lang} Projects`;
 }
 // Event listeners to the different a tags
 addEventListener("load", getAndDisplayAllData);
